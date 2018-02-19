76df5e85a639d04c462f5f13250c187ad399afa6
whiskey2wine <jayakorn.karikan@gmail.com>
2018-02-19 14:43:45 +0700

    Add project page

--- server.js
+++ server.js
@@ -59,6 +59,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handling the request.'
