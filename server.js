56d824a2152df2a9631fd137dd93ca12e4cf025d
whiskey2wine <jayakorn.karikan@gmail.com>
2018-02-19 13:21:58 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+/* jshint esversion: 6 */
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    // res.send('Hello Express!');
+    // res.send({
+    //     name: 'Jayakorn Karikan',
+    //     likes: [
+    //         'Movies',
+    //         'Music',
+    //         'Coding'
+    //     ]
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMsg: 'Welcome to my website'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handling the request.'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000!');
+});
